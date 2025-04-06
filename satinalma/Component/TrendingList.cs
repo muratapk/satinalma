d26f016438f1cea14c9e279a9171535d0abadce3
muratapk <muratapk@hotmail.com>
2025-04-06 18:48:53 +0300

    ana sayfa compoent oluşturulması

---
+++ satinalma/Component/TrendingList.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using satinalma.Data;
+
+namespace satinalma.Component
+{
+    public class TrendingList:ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TrendingList(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var urun = _context.Products.ToList();
+            return View(urun);
+        }
+    }
+}
